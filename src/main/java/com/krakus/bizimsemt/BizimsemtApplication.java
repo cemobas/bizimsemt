addbd2f4d9f12b396fcb546c1c9c86433bccea6c
Cem Onur Bas <cem.bas@sabre.com>
2020-04-04 14:24:15 +0200

    First commit

---
+++ src/main/java/com/krakus/bizimsemt/BizimsemtApplication.java
@@ -0,0 +1,13 @@
+package com.krakus.bizimsemt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizimsemtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizimsemtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krakus/bizimsemt/BizimsemtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krakus.bizimsemt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizimsemtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
