38a2c7a36450e85c5daa76f5f1b340f4ff49d285
cemobas <cemonurbas@yahoo.com>
2020-05-04 13:09:09 +0300

    Add test suite to for client search feature

---
+++ src/test/java/com/krakus/bizimsemt/SearchClientFeatureTestSuite.java
@@ -0,0 +1,14 @@
+package com.krakus.bizimsemt;
+
+import com.krakus.bizimsemt.controller.ClientWebControllerUnitTest;
+import com.krakus.bizimsemt.repository.BuyerRepositoryIntegrationTest;
+import com.krakus.bizimsemt.service.BuyerServiceIntegrationTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses ({ BuyerServiceIntegrationTest.class,
+	BuyerRepositoryIntegrationTest.class, ClientWebControllerUnitTest.class })
+public class SearchClientFeatureTestSuite {
+	// Test Suite Setup (annotations) is sufficient
+}
