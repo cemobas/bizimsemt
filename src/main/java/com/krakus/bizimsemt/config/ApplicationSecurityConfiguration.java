e6e79129bb5c3d58222757424038075b0aced322
cemobas <cemonurbas@yahoo.com>
2020-05-07 22:47:21 +0300

    Add in-memory authentication with UserDetailsService

--- src/main/java/com/krakus/bizimsemt/config/ApplicationSecurityConfiguration.java
+++ src/main/java/com/krakus/bizimsemt/config/ApplicationSecurityConfiguration.java
@@ -2,21 +2,25 @@ package com.krakus.bizimsemt.config;
 
 import com.krakus.bizimsemt.aspect.Loggable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter{
-
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("user").password("{noop}password").roles("USER");
-//    }
 
     @Loggable
     @Override
@@ -24,9 +28,23 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/", "/album", "/css/*", "/js/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> users = new ArrayList<>();
+        users.add(User.withUsername("cemobas").password(passwordEncoder().encode("password")).roles("USER", "ADMIN").build());
+        users.add(User.withUsername("user").password(passwordEncoder().encode("password")).roles("USER").build());
+        return new InMemoryUserDetailsManager(users);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
