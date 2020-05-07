fa8b0927086d9205ab54433157947db1ad19ea29
cemobas <cemonurbas@yahoo.com>
2020-05-07 19:35:05 +0300

    Introduce spring security with predefined user

---
+++ src/main/java/com/krakus/bizimsemt/config/ApplicationSecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.krakus.bizimsemt.config;
+
+import com.krakus.bizimsemt.aspect.Loggable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER");
+    }
+
+    @Loggable
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/index", "/css/*", "/js/*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
